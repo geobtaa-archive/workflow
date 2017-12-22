d16934e527ec4c61139abf09f646182ebeafecd8
zciwej <karenmajewicz@gmail.com>
2017-12-22 14:20:47 -0600

    Added config sample

---
+++ config_sample.py
@@ -0,0 +1,10 @@
+CSW_URL = " " # follow this pattern http://yourgeonetworksite.com/geonetwork/srv/eng/csw-{virtual_csw_name}, if you want virtual csw support. Otherwise just a plain http://yourgeonetworksite.com/geonetwork/srv/eng/csw will work with published records
+CSW_URL_PUB = " " # http(s)://yoursite.com/geonetwork/srv/eng/csw-publication
+CSW_USER = " " # GeoNetwork username
+CSW_PASSWORD = " " # GeoNetwork password
+
+SOLR_USERNAME = " " #Fill in if applicable
+SOLR_PASSWORD = " " #Fill in if applicable
+SOLR_URL = "https://{username}:{password}@ " #  URL for Solr core. Something like http://localhost:8983/solr/blacklightcore/
+
+DEBUG = False # Set this to True if you want more detailed
