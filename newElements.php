d3704e2308ee1e48b16ddc5d9bf6b8dfa9fab3ee
zciwej <karenmajewicz@gmail.com>
2018-07-19 17:20:06 -0500

    new elements

--- newElements.php
+++ newElements.php
@@ -1,5 +1,4 @@
 "b1g_genre_sm": <?php echo(json_encode($genre)); ?>,
-"b1g_geom_type_sm":<?php echo(json_encode($b1gGeomType)); ?>,
 "b1g_centroid_ss": <?php echo(json_encode($centroid)); ?>,
 "b1g_geonames_sm": <?php echo(json_encode($relation)); ?>,
 "b1g_image_ss": <?php echo(json_encode($image)); ?>,
