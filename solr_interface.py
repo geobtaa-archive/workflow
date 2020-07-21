f5eb7bf3454374301f12804a6667eca979dd21a9
karenmajewicz <majew030@umn.edu>
2020-07-21 12:40:09 -0500

    revert to original

--- solr_interface.py
+++ solr_interface.py
@@ -1,10 +1,13 @@
-import pysolr
+#original
+
+mport pysolr
 import json
 
 class SolrInterface(object):
 
-    def __init__(self, url=None):
+    def __init__(self, log=None, url=None):
         self.solr_url = url
+        self.log = log
         self.solr = self._connect_to_solr()
 
     def _connect_to_solr(self):
@@ -24,18 +27,18 @@ class SolrInterface(object):
         self.solr.delete(q='*:*')
 
     def delete_query(self, query, no_confirm=False):
-#         if not no_confirm:
-#             s = self.solr.search(self.escape_query(query), **{"rows": "0"})
-#             are_you_sure = input(
-#                 "Are you sure you want to delete {num_recs} \
-#                 records from Solr? Y/N: ".format(num_recs=s.hits)
-#             )
-#             if are_you_sure.lower() == "y":
-#                 self.solr.delete(q=self.escape_query(query))
-#             else:
-#                 pass#log.debug("Abandon ship! Not deleting anything.")
-#         else:
-        self.solr.delete(q=self.escape_query(query))
+        if not no_confirm:
+            s = self.solr.search(self.escape_query(query), **{"rows": "0"})
+            are_you_sure = raw_input(
+                "Are you sure you want to delete {num_recs} \
+                records from Solr? Y/N: ".format(num_recs=s.hits)
+            )
+            if are_you_sure.lower() == "y":
+                self.solr.delete(q=self.escape_query(query))
+            else:
+                pass#log.debug("Abandon ship! Not deleting anything.")
+        else:
+            self.solr.delete(q=self.escape_query(query))
 
     def json_to_dict(self, json_doc):
         j = json.load(open(json_doc, "r"))
@@ -48,7 +51,5 @@ class SolrInterface(object):
     def add_dict_list_to_solr(self, list_of_dicts):
         try:
             self.solr.add(list_of_dicts)
-            return True
         except pysolr.SolrError as e:
             print("Solr Error: {e}".format(e=e))
-            return False
